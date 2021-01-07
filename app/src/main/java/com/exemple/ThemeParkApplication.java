c2d6318435880b622e4488ed962432ec89e3176b
Moustapha Thiam <moustaphathiam888@gmail.com>
2021-01-07 14:58:06 +0000

    build.gradle correction3

--- app/src/main/java/com/exemple/ThemeParkApplication.java
+++ app/src/main/java/com/exemple/ThemeParkApplication.java
@@ -2,7 +2,6 @@ package com.exemple;
 
 import com.exemple.entity.ThemeParkRide;
 import com.exemple.repository.ThemeParkRideRepository;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
